4d3bc359a0b56994bb76c523fd878d301ece40eb
indrajitbnikam <indrajitbnikam@gmail.com>
2023-01-23 20:48:54 +0530

    feat: add state persist mechanism

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import { Provider } from 'react-redux';
+import KanbanPage from './pages/Kanban';
 import store from './redux/store';
 
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
+
 function App() {
+  const persistor = persistStore(store);
 
   return (
     <Provider store={store}>
-      <h1>
-        DevRev Kanban
-      </h1>
+      <PersistGate persistor={persistor}>
+        <KanbanPage />
+      </PersistGate>
     </Provider>
   )
 }
 
-export default App
+export default App;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,21 @@
 import { configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { PersistConfig, persistReducer } from 'redux-persist';
+import { combineReducers } from '@reduxjs/toolkit';
+
+const persistConfig: PersistConfig<any> = {
+  key: 'root',
+  storage
+};
+
+const reducer = combineReducers({
+
+});
+
+const persistedReducer = persistReducer(persistConfig, reducer);
 
 const store = configureStore({
-  reducer: {}
+  reducer: persistedReducer
 });
 
 export default store;
