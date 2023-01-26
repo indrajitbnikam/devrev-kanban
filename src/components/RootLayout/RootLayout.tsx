c3eba662ede68421dc63db0a075fee2363a4a434
indrajitbnikam <indrajitbnikam@gmail.com>
2023-01-26 18:02:58 +0530

    style: change style as per specs

--- src/components/RootLayout/RootLayout.tsx
+++ src/components/RootLayout/RootLayout.tsx
@@ -5,7 +5,7 @@ type Props = {
 };
 
 function RootLayout({ children }: Props) {
-  return <div className="w-screen h-screen flex flex-col pt-24 bg-[#F9F9F9] text-[#202020]">{children}</div>;
+  return <div className="w-screen min-h-screen h-full flex flex-col pt-24 bg-[#F9F9F9] text-[#202020]">{children}</div>;
 }
 
 export default RootLayout;
--- src/components/modals/CreateLane/CreateLane.tsx
+++ src/components/modals/CreateLane/CreateLane.tsx
@@ -47,11 +47,11 @@ function CreateLane({ onClose }: Props) {
     <Modal title="Create new lane" onClose={onClose}>
       <form className="flex flex-col gap-4">
         <div className="flex flex-col">
-          <label htmlFor="name" className="block mb-2 text-sm font-medium">
+          <label htmlFor="name" className="block mb-2 text-base font-medium">
             Name
           </label>
           <input
-            className="bg-gray-50 border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="bg-gray-50 border border-gray-300 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             id="name"
             type="text"
             placeholder="What's next?"
